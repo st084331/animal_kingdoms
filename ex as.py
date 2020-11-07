edc028e641451b2d1e0886eb300ba85035b4a2b0
st084331 <st084331@student.spbu.ru>
2020-11-08 02:06:41 +0300

    classification v.1

--- ex as.py
+++ ex as.py
@@ -29,4 +29,33 @@ with open('animals - Лист1.csv') as csvfile:
         eterna_growth.append(row['eterna_growth'])
         chlorophyll_type.append(row['chlorophyll_type'])
 
-
+for obj in range(150):
+    if(type_of_diet[obj] == '-'):
+        Viruses.append(obj)
+    elif(type_of_diet[obj] == 'A'):
+        if(core_alability[obj] == '0'):
+            if(presence_of_pseudomurein[obj] == '1'):
+                Archaea.append(obj)
+            else:
+                Bacteria.append(obj)
+        else:
+            if(cyst_formation[obj] == '1'):
+                Protista.append(obj)
+            elif(eterna_growth[obj] == '1'):
+                Plants.append(obj)
+            else:
+                Chromista.append(obj)
+    else:
+        if(cyst_formation[obj] == '1'):
+            if(core_alability[obj] == '1'):
+                Protista.append(obj)
+            else:
+                Bacteria.append(obj)
+        else:
+            if(eterna_growth[obj] == '1'):
+                Fungi.append(obj)
+            else:
+                if(chlorophyll_type[obj] == '-'):
+                    Animals.append(obj)
+                else:
+                    Chromista.append(obj)
