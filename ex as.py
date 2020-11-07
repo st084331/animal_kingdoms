b2a9c66685c64755bc51096d651a0ab3f10d8a22
st084331 <st084331@student.spbu.ru>
2020-11-08 01:41:40 +0300

    csv reader

--- ex as.py
+++ ex as.py
@@ -1,12 +1,4 @@
-list = []
-
-type_of_diet = []
-cyst_formation = []
-chitin = []
-core_alability = []
-presence_of_pseudomurein = []
-eterna_growth = []
-chlorophyll_type = []
+import csv
 
 Bacteria = []
 Archaea = []
@@ -16,4 +8,25 @@ Plants = []
 Animals = []
 Fungi = []
 Viruses = []
+
+type_of_diet = []
+cyst_formation = []
+chitin = []
+core_alability = []
+presence_of_pseudomurein = []
+eterna_growth = []
+chlorophyll_type = []
+
+# Чтение csv файла и рассортировка данных
+with open('animals - Лист1.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        type_of_diet.append(row['type_of_diet'])
+        cyst_formation.append(row['cyst_formation'])
+        chitin.append(row['chitin'])
+        core_alability.append(row['core_alability'])
+        presence_of_pseudomurein.append(row['presence_of_pseudomurein'])
+        eterna_growth.append(row['eterna_growth'])
+        chlorophyll_type.append(row['chlorophyll_type'])
+
 
